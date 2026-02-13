857ec50c0e534cc7baebe9ddfcdb6d78ee328982
rahjreed <rahjreed@gmail.com>
2026-02-12 20:43:31 -0500

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
 
   try {
     const { priceId } = req.body;
+    console.log("CREATE CHECKOUT BODY:", req.body);
 
     if (!priceId) {
       return res.status(400).json({ error: "Missing priceId" });
